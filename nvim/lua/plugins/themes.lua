dbbc2555be1b37e70be5a473df2799a8935bd418
Loyage <792058350@qq.com>
2025-03-26 11:37:26 +0800

    nvim: theme add catppuccin

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,23 @@
+return {
+  -- options (catppuccin-?): latte, frappe, macchiato, mocha
+  -- https://github.com/catppuccin/nvim
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
