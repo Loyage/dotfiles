fb72d9f6966b9e76f8da7560f9f74e99280166aa
Loyage <792058350@qq.com>
2025-03-28 18:16:08 +0800

    nvim: dashboard & avater

--- nvim/lua/plugins/extras/snacks/dashboard.lua
+++ nvim/lua/plugins/extras/snacks/dashboard.lua
@@ -12,30 +12,46 @@ return {
           -- font: ANSI Shadow
           header = [[
 
-██╗      ██████╗ ███████╗██╗   ██╗ █████╗  ██████╗ ███████╗          Z
-██║     ██╔═══██╗╚══███╔╝╚██╗ ██╔╝██╔══██╗██╔════╝ ██╔════╝      Z    
-██║     ██║   ██║  ███╔╝  ╚████╔╝ ███████║██║  ███╗█████╗     z       
-██║     ██║   ██║ ███╔╝    ╚██╔╝  ██╔══██║██║   ██║██╔══╝   z         
-███████╗╚██████╔╝███████╗   ██║   ██║  ██║╚██████╔╝███████╗           
-╚══════╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝           
+██╗      ██████╗ ███████╗██╗   ██╗ █████╗  ██████╗ ███████╗
+██║     ██╔═══██╗╚══███╔╝╚██╗ ██╔╝██╔══██╗██╔════╝ ██╔════╝
+██║     ██║   ██║  ███╔╝  ╚████╔╝ ███████║██║  ███╗█████╗  
+██║     ██║   ██║ ███╔╝    ╚██╔╝  ██╔══██║██║   ██║██╔══╝  
+███████╗╚██████╔╝███████╗   ██║   ██║  ██║╚██████╔╝███████╗
+╚══════╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
 ]],
         },
         sections = {
           { section = "header" },
           { section = "keys", gap = 1, padding = 1 },
           { section = "startup" },
-          -- -- Avater:
-          -- {
-          --   section = "terminal",
-          --   -- https://github.com/TheZoraiz/ascii-image-converter
-          --   -- cmd = "ascii-image-converter <img_path> -C -c",
-          --   -- https://github.com/hpjansson/chafa
-          --   cmd = "chafa <img_path> --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
-          --   -- random = 10,
-          --   pane = 2,
-          --   indent = 4,
-          --   height = 30,
-          -- },
+          -- Avater:
+          { height = 1, pane = 2, section = "terminal", cmd = "echo ' '" },
+          {
+            section = "terminal",
+            pane = 2,
+            -- https://github.com/TheZoraiz/ascii-image-converter
+            -- cmd = "ascii-image-converter 'https://avatars.githubusercontent.com/u/55688916?v=4' -C -c",
+            -- https://github.com/hpjansson/chafa
+            cmd = "chafa ~/.dotfiles/github-avater.png --format symbols --symbols vhalf --size 40x40 ; sleep .1",
+            indent = 12,
+            height = 20,
+          },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
         },
       },
     },
