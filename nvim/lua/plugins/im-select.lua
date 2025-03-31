e5766a91733bb8cddf35e159a95aef79e8c3cff5
Loyage <792058350@qq.com>
2025-03-31 20:10:41 +0800

    nvim: add im-select

---
+++ nvim/lua/plugins/im-select.lua
@@ -0,0 +1,11 @@
+return {
+  -- macos 下的输入法切换
+  {
+    "keaising/im-select.nvim",
+    lazy = false,
+    opts = {
+      default_im_select = "com.apple.keylayout.ABC",
+      default_command = "im-select",
+    },
+  },
+}
