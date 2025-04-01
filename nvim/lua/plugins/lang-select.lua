16de622447fde154f0b6af1bc9785a726f531759
Loyage <792058350@qq.com>
2025-04-01 20:51:31 +0800

    Revert "nvim: 重构lang-select"
    This reverts commit e66cdd55facb1da1daa148a1be9048efeb4fd63d.

--- nvim/lua/plugins/lang-select.lua
+++
@@ -1,7 +0,0 @@
--- Example:
-return {
-  -- Uncomment the following line to enable the language extra you need
-  -- { import = "plugins.extras.lang.python" },
-  -- { import = "plugins.extras.lang.markdown" },
-  -- { import = "plugins.extras.lang.typescript" },
-}
