4ab4bacd58ab015ad55bfd43a5d9db4b4ae05342
Loyage <792058350@qq.com>
2025-04-26 20:32:10 +0800

    nvim: flash rainbow

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  opts = {
+    label = {
+      -- 指示符彩虹🌈底色
+      rainbow = {
+        enabled = true,
+        shade = 5,
+      },
+    },
+  },
+}
