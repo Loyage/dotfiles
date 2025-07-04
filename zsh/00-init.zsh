72b875c68042190a846b8bb555e2a7c8a4d861a9
Loyage <792058350@qq.com>
2025-07-04 19:46:44 +0800

    zsh: add carapace init

--- zsh/00-init.zsh
+++ zsh/00-init.zsh
@@ -10,3 +10,12 @@ export EDITOR=nvim
 export PATH="/usr/lib/ccache/bin/:$PATH"
 export ZSH="$HOME/.oh-my-zsh"
 export XDG_CONFIG_HOME="$HOME/.config"
+
+# -----------------------------------------------------
+# Carapace
+# -----------------------------------------------------
+if command -v carapace &>/dev/null; then
+  export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+  zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+  source <(carapace _carapace)
+fi
