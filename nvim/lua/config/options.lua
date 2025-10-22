1878994b102c340772e982f685fd75020adc3f47
Loyage <792058350@qq.com>
2025-10-22 17:40:24 +0800

    nvim: use OSC 52 to synchronous clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,3 +10,44 @@ end
 if vim.g.vscode then
   require("config.vscode").init()
 end
+
+-- If using Neovim under SSH, using OSC52 to synchronous system clipboard.
+-- In wsl, using clip.exe
+vim.opt.clipboard:append("unnamedplus")
+
+if vim.fn.exists("$SSH_TTY") == 1 and vim.env.TMUX == nil then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+elseif vim.fn.has("wsl") == 1 then
+  ---@diagnostic disable-next-line: param-type-mismatch
+  local script_folder = vim.fs.joinpath(vim.fn.stdpath("config"), "scripts")
+
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      -- WARN: clip.exe might produce garbled text under certain conditions
+      -- ['+'] = 'clip.exe',
+      -- ['*'] = 'clip.exe',
+      ["+"] = "win32yank.exe -i",
+      ["*"] = "win32yank.exe -i",
+    },
+    paste = {
+      -- -- ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      -- -- ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      -- ['+'] = 'win32yank.exe --1f',
+      -- ['*'] = 'win32yank.exe --1f',
+      ["+"] = vim.fs.joinpath(script_folder, "wsl-paste.sh"),
+      ["*"] = vim.fs.joinpath(script_folder, "wsl-paste.sh"),
+    },
+    cache_enabled = true,
+  }
+end
