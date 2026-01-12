6f5a4a5c1642bb0b7f80e7ae54a5862d35d21ca0
Loyage <792058350@qq.com>
2026-01-12 22:43:02 +0800

    zsh add yadm alias

--- .config/zsh/25-aliases.zsh
+++ .config/zsh/25-aliases.zsh
@@ -128,6 +128,19 @@ if command -v gh &>/dev/null; then
   eval "$(gh copilot alias -- bash)"
 fi
 
+# yadm
+if command -v yadm &>/dev/null; then
+  alias y='yadm'
+  alias ys='yadm status'
+  alias ya='yadm add'
+  alias yau='yadm add -u && ys' # update tracked files
+  alias yc='yadm commit -m'
+  alias ypush='yadm push && ys'
+  alias ypull='yadm pull && ysj'
+  alias yls='yadm ls-files'
+  alias ylog='yadm log --oneline --graph --decorate --all'
+fi
+
 # -----------------------------------------------------
 # For Arch Linux
 # -----------------------------------------------------
@@ -138,54 +151,4 @@ if [ "$(uname -s)" = "Linux" ]; then
   alias parus="paru -S"
   alias parur="paru -Rs"
   alias mirrorlist="sudo nvim /etc/pacman.d/mirrorlist"
-
-  # -----------------------------------------------------
-  # Right now, from ml4w-hyprland
-  # -----------------------------------------------------
-  alias shutdown='systemctl poweroff'
-  alias v='$EDITOR'
-  alias vim='$EDITOR'
-  alias ts='~/.config/ml4w/scripts/snapshot.sh'
-  alias wifi='nmtui'
-  alias cleanup='~/.config/ml4w/scripts/cleanup.sh'
-
-  # -----------------------------------------------------
-  # ML4W Apps
-  # -----------------------------------------------------
-  alias ml4w='flatpak run com.ml4w.welcome'
-  alias ml4w-settings='flatpak run com.ml4w.settings'
-  alias ml4w-calendar='flatpak run com.ml4w.calendar'
-  alias ml4w-hyprland='com.ml4w.hyprland.settings'
-  alias ml4w-sidebar='flatpak run com.ml4w.sidebar'
-  alias ml4w-options='ml4w-hyprland-setup -m options'
-  alias ml4w-sidebar='ags toggle sidebar'
-  alias ml4w-diagnosis='~/.config/hypr/scripts/diagnosis.sh'
-  alias ml4w-hyprland-diagnosis='~/.config/hypr/scripts/diagnosis.sh'
-  alias ml4w-qtile-diagnosis='~/.config/ml4w/qtile/scripts/diagnosis.sh'
-  alias ml4w-update='~/.config/ml4w/update.sh'
-
-  # -----------------------------------------------------
-  # Window Managers
-  # -----------------------------------------------------
-
-  alias Qtile='startx'
-  # Hyprland with Hyprland
-
-  # -----------------------------------------------------
-  # Scripts
-  # -----------------------------------------------------
-  alias ascii='~/.config/ml4w/scripts/figlet.sh'
-
-  # -----------------------------------------------------
-  # System
-  # -----------------------------------------------------
-  alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
-
-  # -----------------------------------------------------
-  # Qtile
-  # -----------------------------------------------------
-  alias res1='xrandr --output DisplayPort-0 --mode 2560x1440 --rate 120'
-  alias res2='xrandr --output DisplayPort-0 --mode 1920x1080 --rate 120'
-  alias setkb='setxkbmap de;echo "Keyboard set back to de."'
-
 fi
